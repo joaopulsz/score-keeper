768ec6308a191e7a189fc3f4f033500eb2eff9d0
Joao Pedro Canale Pulsz <joaopulsz@Joaos-MacBook-Pro.local>
2022-05-09 13:18:26 +0100

    Uploading project files to github

---
+++ script.js
@@ -0,0 +1,87 @@
+/*
+    Refactor:
+    - Use const when applicable
+    - Improve variable names so they make more sense.
+    - Create a function to determine the winner
+    - Said function should accept p1 or p2 required arguments
+*/
+
+let victory1 = document.createElement('p');
+let victory2 = document.createElement('p');
+
+const appContainer = document.querySelector('#container');
+
+//declaration of each player's score
+const playerOneScore = document.querySelector('#one-score');
+const playerTwoScore = document.querySelector('#two-score');
+
+let points1 = 0; //playerOnePoints
+let points2 = 0; //playerTwoPoints
+
+playerOneScore.innerText = points1;
+playerTwoScore.innerText = points2;
+
+//variables that represent each players scoring buttons
+const pointPlayerOne = document.querySelector('#one-btn');
+const pointPlayerTwo = document.querySelector('#two-btn');
+
+//variable that represents the match's max score
+let finish;
+
+let maxScore = document.querySelector('#max-points');
+maxScore.addEventListener('change', function () {
+    finish = parseInt(maxScore.value);
+
+    //event for increasing player 1 score count
+    pointPlayerOne.addEventListener('click', function () {
+        points1 += 1;
+        playerOneScore.innerText = points1;
+        playerOneScore.value = points1;
+
+        if (points1 === finish) {
+            victory1.innerText = 'CONGRATULATIONS PLAYER 1!';
+            appContainer.appendChild(victory1);
+
+            pointPlayerOne.disabled = true;
+            pointPlayerTwo.disabled = true;
+        }
+    })
+
+    //event for increasing player 2 score count
+    pointPlayerTwo.addEventListener('click', function () {
+        points2 += 1;
+        playerTwoScore.innerText = points2;
+
+        if (points2 === finish) {
+            victory2.innerText = 'CONGRATULATIONS PLAYER 2!';
+            appContainer.appendChild(victory2);
+
+            pointPlayerOne.disabled = true;
+            pointPlayerTwo.disabled = true;
+        }
+    })
+})
+
+// on reset do the following
+// enable buttons
+// remove game winner message
+
+const resetGameBtn = document.querySelector('#reset');
+reset.addEventListener('click', function () {
+    points1 = 0;
+    points2 = 0;
+
+    playerOneScore.value = "";
+    playerTwoScore.value = "";
+
+    maxScore.value = '';
+
+    pointPlayerOne.disabled = false;
+    pointPlayerTwo.disabled = false;
+
+    victory1.innerText = '';
+    victory2.innerText = '';
+});
+
+
+
